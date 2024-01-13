7c6db5fae2b7f449642470c70b1908dca0293713
AlexWheelock <130195109+AlexWheelock@users.noreply.github.com>
2024-01-12 18:24:00 -0700

    Not working, ask questions

--- Simple Calculator/Module1.vb
+++ Simple Calculator/Module1.vb
@@ -1,6 +1,33 @@
-﻿Module Module1
+﻿'Alex Wheelock
+'RCET 0265
+'Spring 2024
+'Simple Calculator
+'https://github.com/AlexWheelock/Simple-Calculator.git
+
+Module Module1
 
     Sub Main()
+
+        'Creates Variables for User input
+        Dim Number1 As Integer
+        Dim Number2 As Integer
+        Dim Function As String
+
+        Console.WriteLine("Please Enter A Number.") 'prompts the user to define Number1 variable
+        Number1 = Console.ReadLine() 'takes user input
+
+        Console.WriteLine("Please Enter Another Number.") 'prompts the user to define Number2 variable
+        Number2 = Console.ReadLine() 'takes user input
+
+        Console.WriteLine("Would you like the SUM or PRODUCT of these two numbers?")
+        [Function] = Console.ReadLine() 'takes user input to either multiply or add the two numbers
+
+        If Function() = "SUM" Then
+
+        End If
+
+
+
 
     End Sub
 
