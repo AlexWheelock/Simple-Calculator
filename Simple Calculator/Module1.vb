c2d9b996f7f0e0c88303cbbec249de37716ab95f
AlexWheelock <framedabyss8@gmail.com>
2024-01-16 08:55:41 -0700

    Fixed Some Things

--- Simple Calculator/Module1.vb
+++ Simple Calculator/Module1.vb
@@ -4,6 +4,9 @@
 'Simple Calculator
 'https://github.com/AlexWheelock/Simple-Calculator.git
 
+Option Explicit On
+Option Strict On
+
 Module Module1
 
     Sub Main()
@@ -11,23 +14,31 @@ Module Module1
         'Creates Variables for User input
         Dim Number1 As Integer
         Dim Number2 As Integer
-        Dim Function As String
+        Dim Operation As Integer
 
         Console.WriteLine("Please Enter A Number.") 'prompts the user to define Number1 variable
-        Number1 = Console.ReadLine() 'takes user input
+        Number1 = CInt(Console.ReadLine()) 'takes user input
 
         Console.WriteLine("Please Enter Another Number.") 'prompts the user to define Number2 variable
-        Number2 = Console.ReadLine() 'takes user input
+        Number2 = CInt(Console.ReadLine()) 'takes user input
 
-        Console.WriteLine("Would you like the SUM or PRODUCT of these two numbers?")
-        [Function] = Console.ReadLine() 'takes user input to either multiply or add the two numbers
+        Console.WriteLine("Would you like the SUM or PRODUCT of these two numbers?" & vbNewLine _
+                          & "1. SUM" & vbNewLine _
+                          & "2. PRODUCT")
+        Operation = CInt(Console.ReadLine())
 
-        If Function() = "SUM" Then
+        If Operation = CInt("1") Then (Number1 + Number2)
+            Console.WriteLine("Number1 + Number2")
+
+        ElseIf Operation = CInt("2") Then (Number1 * Number2)
+        
+        Else
 
         End If
 
 
 
+        Console.Read()
 
     End Sub
 
